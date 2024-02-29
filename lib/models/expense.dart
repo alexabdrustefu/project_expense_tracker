ba7d2cb15c7592820ca1b9184d5b7dbc1cc9fdb3
alexabdrustefu <alessandro.sstefu06@gmail.com>
2024-02-29 09:54:06 +0100

    add enum property

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,6 +2,8 @@ import 'package:uuid/uuid.dart';
 
 //genera id univoci
 const  uuid=  Uuid();
+//defninisco la classe enum
+enum Category{food, travel, leisure, work}
 
 class Expense {
   //costruttore
@@ -9,11 +11,16 @@ class Expense {
       {
       required this.title,
       required this.prezzo,
-      required this.data}) : id= uuid.v4();
+      required this.data,
+      required this.category}) : id= uuid.v4();
       //con uuid.v4 assegno id univoci alla stringa id
   //definisco proprietá
   final String id;
   final String title;
   final double prezzo;
   final DateTime data;
+  //permette di definire le icone
+  final Category category;
+
+
 }
